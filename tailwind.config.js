ddedadd4b908eb5dbf9f36ee25cad5b600c304b7
Sangharsha <sangamtyata3@gmail.com>
2024-09-17 19:56:40 +0545

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        carribean: "#046865",
+        vista: "#7ea3cc",
+        teagreen: "#CCD5AE",
+        beige: "#E9EDC9",
+        cornsilk: "#FEFAE0",
+        papayawhip: "#FAEDCD",
+        buff: "#D4A373",
+      },
+    },
   },
   plugins: [],
-}
-
+};
